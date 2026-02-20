c18b6e8b3ff975be14d68aa60ded610d35743c71
vjeantet <valere.jeantet@gmail.com>
2026-02-21 00:25:36 +0100

    Update AIPepito to 26.16

--- Casks/aipepito.rb
+++ Casks/aipepito.rb
@@ -1,6 +1,6 @@
 cask "aipepito" do
-  version "26.15"
-  sha256 "18053054e3d93360ef6c9f6d988f188a175990e43ff7c518c927fbf3968fdfb2"
+  version "26.16"
+  sha256 "1d779e0be3b3040a5ffa9d54663395d5e8afded68b21e0f4443873c4d6cf425c"
 
   url "https://github.com/vjeantet/aipepito-releases/releases/download/v#{version}/AIPepito.dmg"
   name "AIPepito"
